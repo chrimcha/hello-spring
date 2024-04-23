f62b197a5d927b135776869de7b8e060ace14d91
Christina Chavez <147195321+chrimcha@users.noreply.github.com>
2024-04-23 18:33:46 -0500

    video 3 set up url query to add name to hello message

--- controllers/HelloController.java
+++ controllers/HelloController.java
@@ -2,6 +2,7 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,11 +11,11 @@ public class HelloController {
     // Handles request at /hello
     // @ResponseBody returns plain text response, will use templates instead later on
     // @GetMapping specifies that this method handles GET requests
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello(){
-        return "Hello, Spring!";
-    }
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
 
     @GetMapping("goodbye")
     @ResponseBody
@@ -22,4 +23,10 @@ public class HelloController {
         return "Goodbye, Spring!";
     }
 
+    // Handles requests of the form /hello?name=LaunchCode
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
 }
