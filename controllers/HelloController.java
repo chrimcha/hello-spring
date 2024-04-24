a6f0ab54514b842301391af3c0e995256efee55f
Christina Chavez <147195321+chrimcha@users.noreply.github.com>
2024-04-23 23:01:37 -0500

    part way down with spring boot exercis, need to finish connecting method to form

--- controllers/HelloController.java
+++ controllers/HelloController.java
@@ -7,48 +7,71 @@ import org.springframework.web.bind.annotation.*;
 @ResponseBody
 @RequestMapping("hello")
 public class HelloController {
+    //handle request at path http://localhost:8080/hello
 
-    // Handles request at /hello
-    // @ResponseBody returns plain text response, will use templates instead later on
-    // @GetMapping specifies that this method handles GET requests
 //    @GetMapping("hello")
+//    @ResponseBody
 //    public String hello(){
 //        return "Hello, Spring!";
 //    }
 
-    // lives at /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye(){
-        return "Goodbye, Spring!";
-    }
+    //handle request at path //http://localhost:8080/hello/goodbye
+//    @GetMapping("goodbye")
+//    public String goodbye(){
+//        return "Goodbye, Spring!";
+//    }
 
-    // lives at /hello/hello
-    // Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping("hello") //handles GET and Post, lives at route "hello"
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }
+    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
+//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+//    public String helloWithQueryParam(@RequestParam String name){
+//        return "Hello, " + name + "!";
+//    }
 
-    // Handles request of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
-    }
+    //handle requests of the form http://localhost:8080/hello/LaunchCode
+//    @GetMapping("{name}")
+//    public String helloWithPathParam(@PathVariable String name){
+//        return "Hello, " + name + "!";
+//    }
 
-    // lives at hello/form
-    // Method to display form
+    //handles requests for http://localhost:8080/hello/form
     @GetMapping("form")
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action='hello' method='post'>" + // submit a request to /hello
-                "<input type='text' name='name' />" +
-                "<input type='submit' value='Greet me!' />" +
+                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
+                "<input type = 'text' name = 'name' >" +
+                "<select name='languages' id='language-select'>" +
+                "<option value='default'>English</option>" +
+                "<option value='french'>French</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='polish'>Polish</option>" +
+                "<option value='thai'>Thai</option>" +
+                "</select>" +
+                "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
 
-
-
+    // Include a new public static method, createMessage, in the HelloController that takes a name as well as a language string. Based on the language string, you’ll display the proper greeting.
+    @GetMapping("")
+    public static String createMessage(@RequestParam String name, @RequestParam String languages) {
+        switch (languages) {
+            case "thai" -> {
+                return "S̄wạs̄dī," + name + "!";
+            }
+            case "polish" -> {
+                return  "Witam," + name + "!";
+            }
+            case "spanish" -> {
+                return "Hola," + name + "!";
+            }
+            case "french" -> {
+                return "Bonjour," + name + "!";
+            }
+            default -> {
+                return "Hel6lo," + name + "!";
+                }
+        }
+    }
 }
