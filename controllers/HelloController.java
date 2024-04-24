de5324b78a105aac22c30c59eaecca625c5e3773
Christina Chavez <147195321+chrimcha@users.noreply.github.com>
2024-04-23 19:10:04 -0500

    video 5 sending form data with request mapping controller

--- controllers/HelloController.java
+++ controllers/HelloController.java
@@ -1,10 +1,7 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -25,7 +22,7 @@ public class HelloController {
     }
 
     // Handles requests of the form /hello?name=LaunchCode
-    @GetMapping("hello")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello") //handles GET and Post, lives at route "hello"
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
@@ -37,5 +34,21 @@ public class HelloController {
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
+
+    // Method to display form
+    @GetMapping("form")
+    @ResponseBody
+    public String helloForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action='hello' method='post'>" + // submit a request to /hello
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet me!' />" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+
 
 }
