25da8567ed926a54c10e7e90c38fb7bd16261646
Christina Chavez <147195321+chrimcha@users.noreply.github.com>
2024-04-25 17:15:37 -0500

    added another solution for languages

--- controllers/HelloController.java
+++ controllers/HelloController.java
@@ -75,4 +75,40 @@ public class HelloController {
             }
         }
     }
+
+    // solution from textbook
+//    @RequestMapping(value="hello", method = RequestMethod.POST)
+//    @ResponseBody
+//    public String helloPost(@RequestParam String name, @RequestParam String language) {
+//        if (name == null) {
+//            name = "World";
+//        }
+//
+//        return createMessage(name, language);
+//
+//        // For a bonus mission, students can change this response text to look nicer.
+//        // This is subjective, but students should be modifying the HTML of the response string.
+//    }
+//
+//    public static String createMessage(String n, String l) {
+//        String greeting = "";
+//
+//        if (l.equals("english")) {
+//            greeting = "Hello";
+//        }
+//        else if (l.equals("french")) {
+//            greeting = "Bonjour";
+//        }
+//        else if (l.equals("italian")) {
+//            greeting = "Bonjourno";
+//        }
+//        else if (l.equals("spanish")) {
+//            greeting = "Hola";
+//        }
+//        else if (l.equals("german")) {
+//            greeting = "Hallo";
+//        }
+//
+//        return greeting + " " + n;
+//    }
 }
